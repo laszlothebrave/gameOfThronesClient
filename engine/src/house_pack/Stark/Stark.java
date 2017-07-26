59b4694fd82b6134fed2f8f229c0a2335fd9c49d
Grzejdziok <mgrzejdziak@gmail.com>
2017-07-26 13:40:56 +0200

    Wersja 1.2

---
+++ engine/src/house_pack/Stark/Stark.java
@@ -0,0 +1,10 @@
+package house_pack.Stark;
+
+import house_pack.House;
+import house_pack.HouseName;
+
+public class Stark extends House{
+    public Stark(int suppliesTrack, int victoryTrack, HouseName name) {
+        super(suppliesTrack, victoryTrack, name);
+    }
+}
