7e7e80d14d3ec78065f8ffdc211a1fa8753c4da4
Leszek <leszekmilasz@gmail.com>
2017-07-26 16:51:40 +0200

    version 1.3

---
+++ Room/src/Room.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Room implements Runnable{
+
+    LinkedBlockingQueue queue;
+    @Override
+    public void run() {
+
+    }
+
+    private void checkmessage() {
+        try {
+            queue.put(queue.take() + " checked");
+        } catch (InterruptedException e){
+//xxxxxs
+        }
+    }
+}
+
