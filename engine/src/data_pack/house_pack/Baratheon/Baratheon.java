1098c980508847d7425fbe6ab0d64a00df68e373
Leszek <leszekmilasz@gmail.com>
2017-08-07 17:59:26 +0200

    version 1.3

---
+++ engine/src/data_pack/house_pack/Baratheon/Baratheon.java
@@ -0,0 +1,18 @@
+package data_pack.house_pack.Baratheon;
+
+import data_pack.Player;
+import data_pack.house_pack.*;
+import exception_pack.UnitTypeLimitReachedException;
+
+public class Baratheon extends House {
+    public Baratheon(Player player) {
+        super(2, 1, HouseName.Baratheon);
+        player.addArea();
+        try {
+            player.getArmy().removeSoldier(2, 1, 2, 0);
+        } catch (UnitTypeLimitReachedException e){
+
+        }
+
+    }
+}
